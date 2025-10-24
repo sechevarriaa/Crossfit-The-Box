949925ee15c2a5a21001f1c08ba8589999b7cd09
sechevarriaa <sechevarria@birt.eus>
2025-10-24 12:32:17 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-// Selecciona el botón hamburguesa y el menú
-const toggleBtn = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.main-nav ul');
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.main-nav ul');
 
-// Agrega el evento click para mostrar/ocultar el menú
-toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('show');
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
 });
-
